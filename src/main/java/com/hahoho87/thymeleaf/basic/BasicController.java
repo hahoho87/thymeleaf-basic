6cc54c2b0e38cd506d07c1e5beceed7541039304
bky4564 <bky4564@hunet.co.kr>
2021-08-09 08:29:59 +0900

    thymeLeaf attribute 제어 추가

--- src/main/java/com/hahoho87/thymeleaf/basic/BasicController.java
+++ src/main/java/com/hahoho87/thymeleaf/basic/BasicController.java
@@ -95,4 +95,9 @@ public class BasicController {
         model.addAttribute("data", "Spring!");
         return "basic/operation";
     }
+
+    @GetMapping("/attribute")
+    public String attribute() {
+        return "basic/attribute";
+    }
 }
